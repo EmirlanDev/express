5d4518a828638f0f69a1bf9d161a539091f91766
EmirlanDev <frdevemirlan@gmail.com>
2025-02-23 16:29:56 +0600

    added express

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+config();
+import express from "express";
+
+export const buildServer = () => {
+  const server = express();
+  server.use(express.json());
+
+  server.get("/", (request, response) => {
+    response.status(200).send({
+      message: "succes",
+    });
+  });
+
+  return server;
+};
